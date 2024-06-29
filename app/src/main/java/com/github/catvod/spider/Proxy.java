da2c1fbdad174e61f6cd4fcb41d1d1dc786afc5a
zengge99 <83830807+zengge99@users.noreply.github.com>
2024-06-29 23:32:38 +0800

    Update Proxy.java

--- app/src/main/java/com/github/catvod/spider/Proxy.java
+++ app/src/main/java/com/github/catvod/spider/Proxy.java
@@ -230,7 +230,9 @@ public class Proxy extends Spider {
     public static Object[] proxy(Map<String, String> params) throws Exception {
         switch (params.get("do")) {
             case "gen":
-                return genProxy(params);
+                
+                return new Object[]{200, "text/plain; charset=utf-8", new ByteArrayInputStream(URLDecoder.decode(params.get("url"), "UTF-8").getBytes("UTF-8"))};
+                //return genProxy(params);
             case "ck":
                 return new Object[]{200, "text/plain; charset=utf-8", new ByteArrayInputStream("ok".getBytes("UTF-8"))};
             case "ali":
