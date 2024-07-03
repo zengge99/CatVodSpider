dfe9e966ef8f9ba721e828c27246442b1686a702
zengge99 <83830807+zengge99@users.noreply.github.com>
2024-07-03 11:50:04 +0800

    Update Proxy.java

--- app/src/main/java/com/github/catvod/spider/Proxy.java
+++ app/src/main/java/com/github/catvod/spider/Proxy.java
@@ -46,6 +46,7 @@ import java.util.concurrent.ArrayBlockingQueue;
 
 class Logger {
     static boolean dbg = false;
+    /*
     public static void log(String message, boolean force) {
         if(!dbg && !force){
             return;
@@ -63,7 +64,24 @@ class Logger {
     public static void log(String message) {
         Logger.log(message, false);
     }
+    */
+
+    public static void log(String message) {
+        if(!dbg){
+            return;
+        }
+        String filePath = "/storage/emulated/0/TV/log.txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+            writer.write(message);
+            writer.newLine();
+            writer.newLine();
+        } catch (IOException e) {
+            System.err.println("Error writing to log file: " + e.getMessage());
+        }
+    }
 }
+
+    
 
 public class Proxy extends Spider {
     private static int port = -1;
@@ -497,7 +515,7 @@ public class Proxy extends Spider {
 
     public void Proxy()
     {
-        Logger.log("代理已加载", true);
+        //Logger.log("代理已加载", true);
     }
 
     public static int getPort() {
