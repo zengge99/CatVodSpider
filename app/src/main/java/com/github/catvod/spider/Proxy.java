dbd7460741ab7df9af5bfacac9fdc271a287593e
zengge99 <83830807+zengge99@users.noreply.github.com>
2024-07-03 08:22:50 +0800

    Update Proxy.java

--- app/src/main/java/com/github/catvod/spider/Proxy.java
+++ app/src/main/java/com/github/catvod/spider/Proxy.java
@@ -156,7 +156,7 @@ public class Proxy extends Spider {
            new ArrayBlockingQueue<>(totalNum));
             //supportRange=false;
             //不支持断点续传，单线程下载
-            if(!this.supportRange) {
+            if(!this.supportRange || threadNum == 1) {
                 Logger.log(connId + "[createDownloadTask]：单线程模式下载，配置线程数：" + threadNum);
                 Future<InputStream> future = this.executorService.submit(() -> {
                     return downloadTask(url, headers, "");
