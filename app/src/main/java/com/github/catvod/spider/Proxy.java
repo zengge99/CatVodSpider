5881c46fee56c9910f178cdb731fe14c82a58b60
zengge99 <83830807+zengge99@users.noreply.github.com>
2024-07-08 14:47:54 +0800

    Update Proxy.java

--- app/src/main/java/com/github/catvod/spider/Proxy.java
+++ app/src/main/java/com/github/catvod/spider/Proxy.java
@@ -42,29 +42,6 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.ArrayBlockingQueue;
 
-/*
-class Logger {
-    static boolean dbg = false;
-    public static void log(String message, boolean force) {
-        if(!dbg && !force){
-            return;
-        }
-        String filePath = "/storage/emulated/0/TV/log.txt";
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
-            writer.write(message);
-            writer.newLine();
-            writer.newLine();
-        } catch (IOException e) {
-            System.err.println("Error writing to log file: " + e.getMessage());
-        }
-    }
-    
-    public static void log(String message) {
-        Logger.log(message, false);
-    }
-}  
-*/
-
 class QurakLinkCacheInfo {
     long cacheTime;
     String cacheLink;
