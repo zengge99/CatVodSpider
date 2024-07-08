2b728a7a3820661d9e69c5aca205808288b110c8
zengge99 <83830807+zengge99@users.noreply.github.com>
2024-07-08 15:32:43 +0800

    Update XiaoyaProxyServer.java

--- app/src/main/java/com/github/catvod/spider/XiaoyaProxyServer.java
+++ app/src/main/java/com/github/catvod/spider/XiaoyaProxyServer.java
@@ -30,14 +30,6 @@ public class XiaoyaProxyServer extends NanoHTTPD {
 
     public XiaoyaProxyServer(int port) {
         super(port);
-        addProcess();
-    }
-
-    private void addProcess() {
-        process = new ArrayList<>();
-        process.add(new Action());
-        process.add(new Cache());
-        process.add(new Local());
     }
 
     public static Response success() {
@@ -68,24 +60,6 @@ public class XiaoyaProxyServer extends NanoHTTPD {
         return proxy(session);
     }
 
-    private void parse(IHTTPSession session, Map<String, String> files) {
-        String ct = session.getHeaders().get("content-type");
-        if (ct != null && ct.toLowerCase().contains("multipart/form-data") && !ct.toLowerCase().contains("charset=")) {
-            Matcher matcher = Pattern.compile("[ |\t]*(boundary[ |\t]*=[ |\t]*['|\"]?[^\"^'^;^,]*['|\"]?)", Pattern.CASE_INSENSITIVE).matcher(ct);
-            String boundary = matcher.find() ? matcher.group(1) : null;
-            if (boundary != null) session.getHeaders().put("content-type", "multipart/form-data; charset=utf-8; " + boundary);
-        }
-        try {
-            session.parseBody(files);
-        } catch (Exception ignored) {
-        }
-    }
-
-    private Response go() {
-        Go.start();
-        return success();
-    }
-
     private Response proxy(IHTTPSession session) {
         try {
             Map<String, String> params = session.getParms();
@@ -94,16 +68,6 @@ public class XiaoyaProxyServer extends NanoHTTPD {
             return rs[0] instanceof Response ? (Response) rs[0] : newChunkedResponse(Response.Status.lookup((Integer) rs[0]), (String) rs[1], (InputStream) rs[2]);
         } catch (Exception e) {
             return error(e.getMessage());
-        }
-    }
-
-    private Response getAssets(String path) {
-        try {
-            if (path.isEmpty()) path = "index.html";
-            InputStream is = Asset.open(path);
-            return newFixedLengthResponse(Response.Status.OK, getMimeTypeForFile(path), is, is.available());
-        } catch (Exception e) {
-            return newFixedLengthResponse(Response.Status.NOT_FOUND, MIME_HTML, null);
         }
     }
 
