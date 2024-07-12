07ea164771a0bf9c1b0972bec2254be51ee8cef1
zengge99 <83830807+zengge99@users.noreply.github.com>
2024-07-12 10:42:07 +0800

    Update XiaoyaProxyServer.java

--- app/src/main/java/com/github/catvod/spider/XiaoyaProxyServer.java
+++ app/src/main/java/com/github/catvod/spider/XiaoyaProxyServer.java
@@ -18,29 +18,6 @@ public class XiaoyaProxyServer extends NanoHTTPD {
         return Loader.INSTANCE;
     }
 
-    public static Response success() {
-        return success("OK");
-    }
-
-    public static Response success(String text) {
-        return newFixedLengthResponse(Response.Status.OK, MIME_PLAINTEXT, text);
-    }
-
-    public static Response error(String text) {
-        return error(Response.Status.INTERNAL_ERROR, text);
-    }
-
-    public static Response error(Response.IStatus status, String text) {
-        return newFixedLengthResponse(status, MIME_PLAINTEXT, text);
-    }
-
-    public static Response redirect(String url, Map<String, String> headers) {
-        Response response = newFixedLengthResponse(Response.Status.REDIRECT, MIME_HTML, "");
-        for (Map.Entry<String, String> entry : headers.entrySet()) response.addHeader(entry.getKey(), entry.getValue());
-        response.addHeader("Location", url);
-        return response;
-    }
-
     @Override
     public Response serve(IHTTPSession session) {
         return proxy(session);
@@ -53,7 +30,7 @@ public class XiaoyaProxyServer extends NanoHTTPD {
             Object[] rs = XiaoyaProxyHandler.proxy(params);
             return rs[0] instanceof Response ? (Response) rs[0] : newChunkedResponse(Response.Status.lookup((Integer) rs[0]), (String) rs[1], (InputStream) rs[2]);
         } catch (Exception e) {
-            return error(e.getMessage());
+            return newFixedLengthResponse(Response.Status.lookup(500), MIME_PLAINTEXT, e.getMessage());
         }
     }
 
