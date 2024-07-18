9b92e02e476e7b961a03281e83387ba492e36d8c
zengge99 <83830807+zengge99@users.noreply.github.com>
2024-07-18 10:25:10 +0800

    Update XiaoyaProxyHandler.java

--- app/src/main/java/com/github/catvod/spider/XiaoyaProxyHandler.java
+++ app/src/main/java/com/github/catvod/spider/XiaoyaProxyHandler.java
@@ -345,6 +345,7 @@ public class XiaoyaProxyHandler {
                     call = downloadClient.newCall(request);
                     response = call.execute();
                     while (!closed && (bytesRead = response.body().byteStream().read(downloadbBuffer)) != -1) {
+                        Logger.log(connId + "[pullDataFromNet]：写入数据：" + range);
                         inputStream.write(downloadbBuffer, 0, bytesRead);
                         clean = false;
                     }
