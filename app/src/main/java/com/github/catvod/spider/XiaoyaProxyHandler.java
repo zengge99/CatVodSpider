cee956183438838350fc3aa6af1d257222c61aeb
zengge99 <83830807+zengge99@users.noreply.github.com>
2024-07-18 20:13:44 +0800

    Update XiaoyaProxyHandler.java

--- app/src/main/java/com/github/catvod/spider/XiaoyaProxyHandler.java
+++ app/src/main/java/com/github/catvod/spider/XiaoyaProxyHandler.java
@@ -449,7 +449,7 @@ public class XiaoyaProxyHandler {
                     Future<InputStream> future = this.executorService.submit(callableQueue.remove());
                     this.is = future.get();
                     */
-                    runTask(100000000/blockSize + 1);
+                    runTask(50 + 1);
                     this.is = this.futureQueue.remove().get();
                     runTask(1);
                     Logger.log(connId + "[read]：读取数据块：" + blockCounter);
